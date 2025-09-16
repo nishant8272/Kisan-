e0c887c0d23b0d869217c482a63fd5b755ffc19e
Aakash tyagi <akashtyagi6065@gmail.com>
2025-09-16 14:17:22 +0530

    add router dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import './App.css'
 import Landing from './components/Landing'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
-      <Landing />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={ <Landing />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
